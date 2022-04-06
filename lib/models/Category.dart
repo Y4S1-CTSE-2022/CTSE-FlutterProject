27499b930957735c1da9ce46bdcfd2a418a4c97c
hansitharaka <hansitharaka1997@gmail.com>
2022-04-06 22:26:24 +0530

    Json extractor in category class

--- lib/models/Category.dart
+++ lib/models/Category.dart
@@ -3,4 +3,13 @@ class Category {
   final String categoryName;
 
   Category(this.id, this.categoryName);
+
+  factory Category.fromJson(dynamic json) {
+    return Category(json['id'] as String, json['category'] as String);
+  }
+
+  @override
+  String toString() {
+    return '{ ${this.id}, ${this.categoryName} }';
+  }
 }
